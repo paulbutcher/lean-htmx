2ec6bda9276f1c1bf56e702efb0bfed3f3576d6f
Paul Butcher <paul@paulbutcher.com>
2026-07-11 21:07:36 +0100

    My first server ;-)

--- Main.lean
+++ Main.lean
@@ -1,2 +1,9 @@
-def main : IO Unit :=
-  IO.println s!"Hello, world!"
+import Std.Http.Server
+
+open Std Async
+open Std Http Server Response
+
+def main : IO Unit := Async.block do
+  let addr : Net.SocketAddress := .v4 ⟨.ofParts 127 0 0 1, 8080⟩
+  let server ← Server.serve addr (Handler.ofFn fun _ => Response.ok |>.text "Hey there ;-)")
+  server.waitShutdown
