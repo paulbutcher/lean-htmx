a77aa1cfc01ac6dbdcb0f2477219e01ad5e250ac
Paul Butcher <paul@paulbutcher.com>
2026-07-12 13:43:12 +0100

    Remove unnecessary open

--- Main.lean
+++ Main.lean
@@ -1,7 +1,7 @@
 import Std.Http.Server
 
 open Std Async
-open Std Http Server Response
+open Std Http Server
 
 def main : IO Unit := Async.block do
   let addr : Net.SocketAddress := .v4 ⟨.ofParts 127 0 0 1, 8080⟩
